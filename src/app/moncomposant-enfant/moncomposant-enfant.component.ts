ce6cf414a28bad60622c5df69ab83ffce59c1b04
CMM <chrystellemm@gmail.com>
2021-07-23 14:43:01 +0200

    ain

--- src/app/moncomposant-enfant/moncomposant-enfant.component.ts
+++ src/app/moncomposant-enfant/moncomposant-enfant.component.ts
@@ -1,21 +1,29 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-moncomposant-enfant',
-  template: `<h1>{{title}}</h1>`,
+  template: `<h1 (click)="declencheCallEvent()">{{title}}</h1>`,
   styleUrls: ['./moncomposant-enfant.component.css']
 })
-export class MoncomposantEnfantComponent implements OnInit {
-
+export class MoncomposantEnfantComponent implements OnInit,OnChanges {
+  /**
+   * title devient un attribut personnalisé html de la balise du composant
+   * pour faire le lien avec une donnée du composant parent
+   * on utilise les [] pour récupérer la variable ???
+   * on utilise un evénement personnalisé callEvent grâce à @Output
+   * on lui donne le nom d'une méthode avec en paramètre $event (qui est l'injection d'évènement d'Angular)
+   * <app-moncomposant-enfant [title]="???" (callEvent)="!!!($event)"></app-moncomposant-enfant>
+   */
   @Input() title : string;
-
-  constructor() {console.log("CONSTRUCTOR") }
-
-  ngOnChange(changes: SimpleChanges): void {
-    console.log("OnChange");
+  @Output() callEvent : EventEmitter<string> = new EventEmitter<string>();
+  constructor() { console.log("CONSTRUCTOR")}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("OnChanges");
   }
   ngOnInit(): void {
     console.log("OnInit");
   }
-
+  declencheCallEvent() : void  {
+    this.callEvent.emit("Mon enfant !");
+  }
 }
--- src/app/moncomposant/moncomposant.component.ts
+++ src/app/moncomposant/moncomposant.component.ts
@@ -3,38 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-moncomposant',
   template: `<div [align]="alignement">
-             <h1>{{uneChose}}</h1>
-             <h2 [hidden]="masque">{{unObjet?.nom}}{{unObjet.prenom}}</h2>
-            <h2 *ngIf="!masque">{{unObjet.nom}} {{unObjet.prenom}}</h2>
-            <input type="text" [disabled]="masque" value="{{unObjet.nom}}" #saisieNom>
-            <h3 (click)="saisieNom.focus()">{{saisieNom.value}}</h3>
-
-            <button [disabled]= "masque" (click)="masquer()">VALIDER</button>
-
-            <button (click)="afficher()">AFFICHER</button>
-            <hr>
-            <input type="text" [(ngModel)]="unObjet.prenom">
-            <span>{{unObjet.prenom}}</span>
+                  <h1>{{uneChose}}</h1>
+                  <h2 [hidden]="masque">{{unObjet?.nom}} {{unObjet?.prenom}}</h2>
+                  <h2 *ngIf="!masque">{{unObjet?.nom}} {{unObjet?.prenom}}</h2>
+                  <input type="text" [disabled]="masque" [value]="unObjet.nom" #saisieNom>
+                  <h3 (click)="saisieNom.focus()">{{saisieNom.value}}</h3>
+                  <button [disabled]="masque" (click)="masquer()">VALIDER</button>
+                  <button (click)="afficher()">AFFICHER</button>
+                  <hr/>
+                  <input type="text" [(ngModel)]="unObjet.prenom">
+                  <span>{{unObjet.prenom}}</span>
+                  <hr/>
+                  <app-moncomposant-enfant [title]="unObjet.prenom"
+                    (callEvent)="emitCallEvent($event)">
+                  </app-moncomposant-enfant>
+                  <hr/>
+                  <h4>{{retourCallEvent}}</h4>
             </div>`,
-
-
   styleUrls: ['./moncomposant.component.css']
 })
 export class MoncomposantComponent implements OnInit {
-
-  public uneChose:string = 'UNE CHOSE'
-  public unObjet:any = {nom:'M-M', prenom:'Chrystelle'}
+/**
+ * Interpolation {{uneChose}} ou {{this.uneChose}}
+ */
+  public uneChose:string = 'UNE CHOSE';
+  public unObjet:any = {nom:'Germain', prenom: "Christophe" };
   public alignement:string = 'center';
   public masque:boolean = true;
-  constructor() { }
+
+  //Pour le callEvent
+  public retourCallEvent:string = 'vide';
+  /**
+   * Méthode qui va être appelé par le composant enfant
+   * le paramètre message c'est la variable passée dans
+   * la méthode emit("Mon enfant !")
+   */
+  emitCallEvent(message:string) : void {
+      this.retourCallEvent = message;
+  }
+
+ constructor() { }
 
   ngOnInit(): void {
   }
-afficher() : void{
-  this.masque = false;
-}
-masquer() : void{
-  this.masque = true;
-}
-
+  afficher() : void {
+    this.masque = false;
+  }
+  masquer() : void {
+    this.masque = true;
+  }
 }
